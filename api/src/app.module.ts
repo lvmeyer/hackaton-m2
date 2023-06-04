87d58e13711b98a0213f9984c51de69ec75b7357
DanLevyM <dan.levy.pro@gmail.com>
2023-06-04 17:33:09 +0200

    api - connection to db

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmCustomModule } from './typeorm/typeorm.module.js';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { User } from './user/User.js';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+      }),
+      envFilePath: './.env',
+    }),
+    TypeOrmCustomModule.register(),
+    TypeOrmModule.forFeature([User]),
+    UserModule,
+  ],
+  controllers: [UserController],
+  providers: [],
 })
 export class AppModule {}
---
+++ api/src/user/User.ts
@@ -0,0 +1,27 @@
+import { IsEmail } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @UpdateDateColumn()
+  updatedDate: Date;
+}
