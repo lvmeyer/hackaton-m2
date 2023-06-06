c2d78889a27e56a352d5733b176ac2b1ea14f12b
DanLevyM <dan.levy.pro@gmail.com>
2023-06-06 15:50:52 +0200

    router

--- front/src/Components/Navbar.tsx
+++ front/src/Components/Navbar.tsx
@@ -2,27 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Accueil</Link>
-        </li>
-        <li>
-          <Link to="/formation">Formation</Link>
-        </li>
-        <li>
-          <Link to="/users">Utilisateurs</Link>
-        </li>
-        <li>
-          <Link to="/login">Connexion</Link>
-        </li>
-        <li>
-          <Link to="/register">Inscription</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link to="/home">Accueil</Link>
+				</li>
+				{/* <li>
+					<Link to="/formation">Formation</Link>
+				</li> */}
+				<li>
+					<Link to="/users">Utilisateurs</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+};
 
 export default Navbar;
