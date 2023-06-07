13cb0371187b68833fcc1a5ead19a98ef1e2828f
Ulyssee <ulysse.mf@hotmail.fr>
2023-06-07 11:35:06 +0200

    add dev profil

---
+++ front/src/Components/DevPage/TableauFormations.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const TableauFormations: React.FC = () => {
+  const [tabIndex, setTabIndex] = useState(0); // Index du tableau actuellement affiché
+
+  const formationsAVenir = ['Formation 1', 'Formation 2', 'Formation 3'];
+  const formationsTerminees = ['Formation A', 'Formation B', 'Formation C'];
+
+  const switchTab = (index: number) => {
+    setTabIndex(index);
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col">
+          <button
+            className={`btn btn-primary me-2 ${
+              tabIndex === 0 ? 'active' : ''
+            }`}
+            onClick={() => switchTab(0)}
+          >
+            Formations à venir
+          </button>
+          <button
+            className={`btn btn-primary ${
+              tabIndex === 1 ? 'active' : ''
+            }`}
+            onClick={() => switchTab(1)}
+          >
+            Formations terminées
+          </button>
+        </div>
+      </div>
+
+      <div className="row mt-3">
+        <div className="col">
+          {tabIndex === 0 ? (
+            <ul className="list-group">
+              {formationsAVenir.map((formation, index) => (
+                <li key={index} className="list-group-item">
+                  {formation}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <ul className="list-group">
+              {formationsTerminees.map((formation, index) => (
+                <li key={index} className="list-group-item">
+                  {formation}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TableauFormations;
--- front/src/Vues/Developer.tsx
+++ front/src/Vues/Developer.tsx
@@ -1,72 +1,55 @@
 import React from 'react';
 import { FaJs, FaJava, FaPython } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import profilimg from '../../public/img/dev.jpg';
+import Badge from '../Components/DevPage/Badge';
+import TableauFormations from '../Components/DevPage/TableauFormations';
 
 const Developer: React.FC = () => {
+  const isAvailable = true; // Remplacez par la variable de disponibilité réelle du développeur
+
   return (
-    <div className="container d-flex align-items-center">
-      <div className="col d-flex flex-column align-items-center">
-      <div
-          className="rounded-circle overflow-hidden"
-          style={{ width: '250px', height: '250px' }}
-        >
-          <img
-            src="../../public/img/dev.jpg"
-            alt="Profil"
-            className="img-fluid"
-            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-          />
-        </div>
-        <h2>Oh le J t'y as mit le Kimono ?</h2>
-      </div>
+    <div className="container dev-profil-section">
+      <div className='d-flex align-items-center justify-content-around '>
+        <div className="d-flex flex-column align-items-center">
+          <div
+            className="rounded-circle overflow-hidden"
+            style={{ width: '250px', height: '250px' }}
+          >
+            <img
+              src={profilimg}
+              alt="Profil"
+              className="img-fluid"
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+          </div>
+          <h2 className="mt-3">Richard Blyat</h2>
 
-      <div className="col">
-        <h3>Badges :</h3>
-        <div className="d-flex align-items-center">
-          <FaJs className="me-2" />
-          <span>JavaScript</span>
-          <div className="progress flex-grow-1">
+          <div className="d-flex align-items-center">
+            <p className="align-self-center">Disponibilité :</p>
             <div
-              className="progress-bar"
-              role="progressbar"
-              style={{ width: '80%' }}
-              aria-valuenow={80}
-              aria-valuemin={0}
-              aria-valuemax={100}
+              className={`ms-2 rounded-circle ${
+                isAvailable ? 'bg-success' : 'bg-success border border-dark'
+              }`}
+              style={{ width: '10px', height: '10px' }}
             ></div>
           </div>
         </div>
 
-        <div className="d-flex align-items-center">
-          <FaJava className="me-2" />
-          <span>Java</span>
-          <div className="progress flex-grow-1">
-            <div
-              className="progress-bar"
-              role="progressbar"
-              style={{ width: '60%' }}
-              aria-valuenow={60}
-              aria-valuemin={0}
-              aria-valuemax={100}
-            ></div>
+        <div className="d-flex col flex-column align-items-center">
+          <div className='d-flex flex-row '> 
+            <Badge />
+            <Badge />
           </div>
-        </div>
-
-        <div className="d-flex align-items-center">
-          <FaPython className="me-2" />
-          <span>Python</span>
-          <div className="progress flex-grow-1">
-            <div
-              className="progress-bar"
-              role="progressbar"
-              style={{ width: '40%' }}
-              aria-valuenow={40}
-              aria-valuemin={0}
-              aria-valuemax={100}
-            ></div>
+          <div className='d-flex flex-row '> 
+            <Badge />
+            <Badge />
           </div>
         </div>
       </div>
+
+
+      <TableauFormations />
     </div>
   );
 };
